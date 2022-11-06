32506240d38f6c7bfa2b4525329c8ef5a2c2862f
amersinger21 <amersinger21@verizon.net>
2022-11-06 15:59:03 -0500

    add real links and create contact form page

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const ToggleNavBtn = document.querySelector("#nav-display-btn");
-const NavLinks = document.querySelectorAll(".nav-link");
+const toggleNavBtn = document.getElementById("nav-display-btn");
+const navLinks = document.querySelectorAll(".nav-link");
 
 function ToggleNav() {
-  NavLinks.forEach((item) => {
+  navLinks.forEach((item) => {
     if (item.style.display === "none") {
       item.style.display = "block";
     } else {
@@ -11,4 +11,27 @@ function ToggleNav() {
   });
 }
 
-ToggleNavBtn.addEventListener("click", ToggleNav);
+function SendEmail() {
+  document.getElementById("email-form").action = "";
+
+  if (!document.getElementById("name").value.length > 0) {
+    return;
+  }
+
+  if (!document.getElementById("msg").value.length > 0) {
+    return;
+  } else {
+    window.location.href =
+      "mailto:amersinger21@gmail.com?subject=New%20Contact%20From%20" +
+      document.getElementById("name").value +
+      "&body=" +
+      document.getElementById("msg").value;
+
+    window.location.reload();
+  }
+}
+
+toggleNavBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  ToggleNav;
+});
